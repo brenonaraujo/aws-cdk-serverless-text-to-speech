e1e028c7142d2f6bbe3c47d0f8116ce634f3cc8d
brenonaraujo <brenonaraujo@gmail.com>
2023-07-11 09:14:39 -0300

    feat: Creation of the main stack with textBucket, audioBucket, metadataTable and processingQueue infrastructure

--- lib/aws-cdk-serverless-text-to-speech-stack.ts
+++ lib/aws-cdk-serverless-text-to-speech-stack.ts
@@ -1,16 +1,35 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsCdkServerlessTextToSpeechStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const textBucket = new s3.Bucket(this, 'gptts-text-bucket', {
+      versioned: false,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkServerlessTextToSpeechQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const audioBucket = new s3.Bucket(this, 'gptts-audio-bucket', {
+      versioned: false,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+    });
+
+    const metadataTable = new dynamodb.Table(this, 'MetadataTable', {
+      partitionKey: { name: 'uuid', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'submissionTime', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
+
+    const processingQueue = new sqs.Queue(this, 'ProcessingQueue', {
+      visibilityTimeout: cdk.Duration.seconds(300), // 5 minutes
+    });
+
   }
 }
